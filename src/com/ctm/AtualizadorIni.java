0bc3ea9954bc27460e1043c2d4d9ad1d9cde1620
dsfca-iscteiul <dsfca@iscte-iul.pt>
2020-05-25 12:08:46 +0100

    AtualizadorIni: class created

---
+++ src/com/ctm/AtualizadorIni.java
@@ -0,0 +1,14 @@
+package com.ctm;
+
+public class AtualizadorIni {
+
+	public AtualizadorIni() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
