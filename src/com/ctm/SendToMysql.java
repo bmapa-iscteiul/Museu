30123644bccac5355e528285b5573a0515e23222
bmapa-iscteiul <bmapa@iscte-iul.pt>
2020-05-28 14:35:55 +0100

    Sending email

--- src/com/ctm/SendToMysql.java
+++ src/com/ctm/SendToMysql.java
@@ -1,6 +1,10 @@
 package com.ctm;
 
 import java.io.FileInputStream;
+import java.util.*;
+import javax.mail.*;
+import javax.mail.internet.*;
+import javax.activation.*;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.sql.Connection;
@@ -75,10 +79,14 @@ public class SendToMysql extends Thread {
 						") values ('"+a.getDataHora()+"','"+a.getTipoSensor()+"','"+a.getValor()+"','"+a.getLimit()+"','"+a.getDescricao()+"','"+a.getControlo()+"');";
 				mySqlstatements.executeUpdate(SqlCommando);
 			}
-		}catch (Exception e)
-		{
-		System.out.println("Error quering  the database . " + e);
-		//enviar email
+		}catch (Exception e){
+		System.out.println("Error quering  the database . Now sending emails. " + e);
+			for(Alerta a: alertas) {
+				String to =MainMongoToMySql.getMysqlProperty("emergencyEmail");
+				String subject = a.getDescricao();
+				String text = a.getDataHora()+"','"+a.getTipoSensor()+"','"+a.getValor()+"','"+a.getLimit()+"','"+a.getDescricao()+"','"+a.getControlo();
+				sendEmail(to, subject, text);
+			}
 		}
 	}
 	
@@ -91,7 +99,28 @@ public class SendToMysql extends Thread {
 		return migrationTime;
 	}
 	
-	
+	public void sendEmail(String to, String subject, String text) {
+	     String from = "bruno-porto@live.com.pt";
+	     String host = "localhost";
+	     Properties properties = System.getProperties();
+	     properties.setProperty("mail.smtp.host", host);
+	     Session session = Session.getDefaultInstance(properties);
+
+	     try {
+	         MimeMessage message = new MimeMessage(session);
+
+	         message.setFrom(new InternetAddress(from));
+	         message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+	         message.setSubject(subject);
+	         message.setText(text);
+
+	         Transport.send(message);
+	         System.out.println("Sent message successfully....");
+	      } catch (MessagingException mex) {
+	         mex.printStackTrace();
+	      }
+	     
+	}
 	
 
 }
