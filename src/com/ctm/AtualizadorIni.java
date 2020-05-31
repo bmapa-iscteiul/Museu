4a797938183f27683795fe4704435bf8de3cd762
bmapa-iscteiul <bmapa@iscte-iul.pt>
2020-05-31 11:48:30 +0100

    atualiza mongotomysql e cloudtomongo

--- src/com/ctm/AtualizadorIni.java
+++ src/com/ctm/AtualizadorIni.java
@@ -1,14 +1,42 @@
 package com.ctm;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
 public class AtualizadorIni {
 
+	static Properties MongoToMysqlIni = new Properties();
+	static Properties CloudToMongoIni = new Properties();
+	
 	public AtualizadorIni() {
-		// TODO Auto-generated constructor stub
+		loadMongoToMysqlIni();
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
  
 	}
+	
+	public void atualizarCampoMongoToMysqlIni(String key, String value) {
+		MongoToMysqlIni.setProperty(key, value);
+	}
+	
+	public void atualizarCampoCloudToMongoIni(String key, String value) {
+		CloudToMongoIni.setProperty(key, value);
+	}
 
+	public static void loadMongoToMysqlIni() {
+		try {
+				MongoToMysqlIni.load(new FileInputStream("MongoToMysql.ini"));
+		} catch (FileNotFoundException  e) { e.printStackTrace();
+		} catch (IOException e2) { e2.printStackTrace(); }
+	}
+	
+	public static void loadCloudToMongoIni() {
+		try {
+				CloudToMongoIni.load(new FileInputStream("CloudToMongo.ini"));
+		} catch (FileNotFoundException  e) { e.printStackTrace();
+		} catch (IOException e2) { e2.printStackTrace(); }
+	}
 }
