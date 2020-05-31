2760c77e6281e05e25d07ba39d8cb1aabe957764
bmapa-iscteiul <bmapa@iscte-iul.pt>
2020-05-31 12:04:58 +0100

    atualiza mysql

--- src/com/ctm/AtualizadorIni.java
+++ src/com/ctm/AtualizadorIni.java
@@ -3,12 +3,20 @@ package com.ctm;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.time.LocalTime;
 import java.util.Properties;
 
 public class AtualizadorIni {
 
 	static Properties MongoToMysqlIni = new Properties();
 	static Properties CloudToMongoIni = new Properties();
+	
+	private Connection mySqlConnection;
+	private Statement mySqlstatements;
 	
 	public AtualizadorIni() {
 		loadMongoToMysqlIni();
@@ -38,5 +46,30 @@ public class AtualizadorIni {
 				CloudToMongoIni.load(new FileInputStream("CloudToMongo.ini"));
 		} catch (FileNotFoundException  e) { e.printStackTrace();
 		} catch (IOException e2) { e2.printStackTrace(); }
+	}
+	
+	private void connectToMysql() {
+		System.out.println(LocalTime.now().toString());
+		String database_password = MainMongoToMySql.getMysqlProperty("database_password");
+		String database_user = MainMongoToMySql.getMysqlProperty("database_user");
+		String database_connection = MainMongoToMySql.getMysqlProperty("mysql_host");
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
+			mySqlConnection =  DriverManager.getConnection(database_connection,database_user,database_password);
+			mySqlstatements = this.mySqlConnection.createStatement();
+		} catch (Exception e) {
+			System.out.println("Server down, unable to make the connection. ");
+		}
+	} 
+	
+	public void atualizaMysql(String key, String value) {
+		connectToMysql();
+		String sql_query = "UPDATE sistema SET " + key + " = " + "'" + value + "';";
+		try {
+			mySqlstatements.executeUpdate(sql_query);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
